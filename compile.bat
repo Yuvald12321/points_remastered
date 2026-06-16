359467e2fea978ef5dc72534a7613978e93d78e9
Yuval <yuvalivshits@gmail.com>
2026-06-16 19:16:42 +0300

    bat

--- compile.bat
+++ compile.bat
@@ -1,2 +1,5 @@
 pyinstaller --noconsole --onefile --icon=logo.ico -n "אוצרות או צרות" game.py
 pyinstaller --noconsole --onefile --add-data "dist/אוצרות או צרות.exe;." --add-data "אוצרות או צרות.html;." -n "אוצרות או צרות installer" installer.py
+rd /S /Q "build"
+del /Q "אוצרות או צרות.spec"
+del /Q "אוצרות או צרות installer.spec"
